7df7bb4cbad05d73409cece6f7268e27259206f6
zhaojiue34 <2827871771@qq.com>
2024-05-03 22:47:09 +0800

    5_3

---
+++ 24_5_3/24_5_3.cpp
@@ -0,0 +1,280 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+void Test()
+{
+	int* p1 = (int*)malloc(sizeof(int));
+	free(p1);
+	// 1.malloc/calloc/realloc的区别是什么？
+	int* p2 = (int*)calloc(4, sizeof(int));
+	int* p3 = (int*)realloc(p2, sizeof(int) * 10);
+	// 这里需要free(p2)吗？
+	free(p3);
+}
+
+void Test()
+{
+	// 动态申请一个int类型的空间
+	int* ptr4 = new int;
+	// 动态申请一个int类型的空间并初始化为10
+	int* ptr5 = new int(10);
+	// 动态申请10个int类型的空间
+	int* ptr6 = new int[10];
+	delete ptr4;
+	delete ptr5;
+	delete[] ptr6;
+}
+
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		: _a(a)
+//	{
+//		cout << "A():" << this << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A():" << this << endl;
+//	}
+//private:
+//	int _a;
+//};
+//int main()
+//{
+//	// new/delete 和 malloc/free最大区别是 new/delete对于【自定义类型】除了开空间还会调用构
+//	//造函数和析构函数
+//	A* p1 = (A*)malloc(sizeof(A));
+//	A* p2 = new A(1);
+//	free(p1);
+//	delete p2;
+//	// 内置类型是几乎是一样的
+//	int* p3 = (int*)malloc(sizeof(int)); // C
+//	int* p4 = new int;
+//	free(p3);
+//	delete p4;
+//	A* p5 = (A*)malloc(sizeof(A) * 10);
+//	A* p6 = new A[10];
+//	free(p5);
+//	delete[] p6;
+//	return 0;
+//}
+
+
+///*
+//operator new：该函数实际通过malloc来申请空间，当malloc申请空间成功时直接返回；申请空间失败，
+//尝试执行空 间不足应对措施，如果改应对措施用户设置了，则继续申请，否则抛异常。
+//*/
+//void* __CRTDECL operator new(size_t size) _THROW1(_STD bad_alloc)
+//{
+//	// try to allocate size bytes
+//	void* p;
+//	while ((p = malloc(size)) == 0)
+//		if (_callnewh(size) == 0)
+//		{
+//			// report no memory
+//			// 如果申请内存失败了，这里会抛出bad_alloc 类型异常
+//			static const std::bad_alloc nomem;
+//			_RAISE(nomem);
+//		}
+//	return (p);
+//}
+///*
+//operator delete: 该函数最终是通过free来释放空间的
+//*/
+//void operator delete(void* pUserData)
+//{
+//	_CrtMemBlockHeader* pHead;
+//	RTCCALLBACK(_RTC_Free_hook, (pUserData, 0));
+//	29
+//	if (pUserData == NULL)
+//			return;
+//	_mlock(_HEAP_LOCK); /* block other threads */
+//	__TRY
+//		/* get a pointer to memory block header */
+//		pHead = pHdr(pUserData);
+//	/* verify block type */
+//	_ASSERTE(_BLOCK_TYPE_IS_VALID(pHead->nBlockUse));
+//	_free_dbg(pUserData, pHead->nBlockUse);
+//	__FINALLY
+//		_munlock(_HEAP_LOCK); /* release other threads */
+//	__END_TRY_FINALLY
+//		return;
+//}
+//
+///*
+//free的实现
+//*/
+//#define free(p) _free_dbg(p, _NORMAL_BLOCK)
+
+
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		: _a(a)
+//	{
+//		cout << "A():" << this << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A():" << this << endl;
+//	}
+//private:
+//	int _a;
+//};
+//// 定位new/replacement new
+//int main()
+//{
+//	// p1现在指向的只不过是与A对象相同大小的一段空间，还不能算是一个对象，因为构造函数没有执行
+//	A* p1 = (A*)malloc(sizeof(A));
+//	new(p1)A; // 注意：如果A类的构造函数有参数时，此处需要传参
+//	p1->~A();
+//	free(p1);
+//	A* p2 = (A*)operator new(sizeof(A));
+//	new(p2)A(10);
+//	p2->~A();
+//	operator delete(p2);
+//	return 0;
+//}
+
+void Swap(int& left, int& right)
+{
+	int temp = left;
+	left = right;
+	right = temp;
+}
+void Swap(double& left, double& right)
+{
+	double temp = left;
+	left = right;
+	right = temp;
+}
+void Swap(char& left, char& right)
+{
+	char temp = left;
+	left = right;
+	right = temp;
+}
+
+template<typename T>
+void Swap(T& left, T& right)
+{
+	T temp = left;
+	left = right;
+	right = temp;
+}
+
+//template<class T>
+//T Add(const T& left, const T& right)
+//{
+//	return left + right;
+//}
+//int main()
+//{
+//	int a1 = 10, a2 = 20;
+//	double d1 = 10.0, d2 = 20.0;
+//	Add(a1, a2);
+//	Add(d1, d2);
+//
+//	/*
+//	该语句不能通过编译，因为在编译期间，当编译器看到该实例化时，需要推演其实参类型
+//	通过实参a1将T推演为int，通过实参d1将T推演为double类型，但模板参数列表中只有一个T，
+//	编译器无法确定此处到底该将T确定为int 或者 double类型而报错
+//	注意：在模板中，编译器一般不会进行类型转换操作，因为一旦转化出问题，编译器就需要背黑锅
+//	Add(a1, d1);
+//	*/
+//
+//	// 此时有两种处理方式：1. 用户自己来强制转化 2. 使用显式实例化
+//	Add(a, (int)d);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int a = 10;
+//	double b = 20.0;
+//
+//	// 显式实例化
+//	Add<int>(a, b);
+//	return 0;
+//}
+
+//// 专门处理int的加法函数
+//int Add(int left, int right)
+//{
+//	return left + right;
+//}
+//// 通用加法函数
+//template<class T>
+//T Add(T left, T right)
+//{
+//	return left + right;
+//}
+//void Test()
+//{
+//	Add(1, 2); // 与非模板函数匹配，编译器不需要特化
+//	Add<int>(1, 2); // 调用编译器特化的Add版本
+//}
+
+//// 专门处理int的加法函数
+//int Add(int left, int right)
+//{
+//	return left + right;
+//}
+//// 通用加法函数
+//template<class T1, class T2>
+//T1 Add(T1 left, T2 right)
+//{
+//	return left + right;
+//}
+//void Test()
+//{
+//	Add(1, 2); // 与非函数模板类型完全匹配，不需要函数模板实例化
+//	Add(1, 2.0); // 模板函数可以生成更加匹配的版本，编译器根据实参生成更加匹配的Add函
+//	//数
+//}
+
+
+// 动态顺序表
+// 注意：Vector不是具体的类，是编译器根据被实例化的类型生成具体类的模具
+template<class T>
+class Vector
+{
+public:
+	Vector(size_t capacity = 10)
+		: _pData(new T[capacity])
+		, _size(0)
+		, _capacity(capacity)
+	{}
+
+	// 使用析构函数演示：在类中声明，在类外定义。
+	~Vector();
+
+	void PushBack(const T& data)；
+		void PopBack()；
+		// ...
+
+		size_t Size() { return _size; }
+
+	T& operator[](size_t pos)
+	{
+	assert(pos < _size);
+	return _pData[pos];
+	}
+
+private:
+	T* _pData;
+	size_t _size;
+	size_t _capacity;
+};
+// 注意：类模板中函数放在类外进行定义时，需要加模板参数列表
+template <class T>
+Vector<T>::~Vector()
+{
+	if (_pData)
+		delete[] _pData;
+	_size = _capacity = 0;
+}
