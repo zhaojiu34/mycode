f805eae8f600a9e1f6f26f4061760af20e2b9489
zhaojiue34 <2827871771@qq.com>
2024-06-20 21:24:45 +0800

    6.20

---
+++ 24.6.20/源.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n == 0 || n < 0)
+            return false;
+
+        while (n > 1)
+        {
+            if (n % 2 != 0)
+            {
+                return false;
+            }
+
+            n /= 2;
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+
+    cout << Solution().isPowerOfTwo(16);
+
+
+    return 0;
+}
+
+
+int main()
+{
+
+    cout << Solution().isPowerOfTwo(16);
+
+
+    return 0;
+}
