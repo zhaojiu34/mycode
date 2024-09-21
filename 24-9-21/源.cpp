1b8bbea0d5d8329cac6838c7dbfe4f57d803cd7a
zhaojiue34 <2827871771@qq.com>
2024-09-21 20:35:26 +0800

    24-9-21

---
+++ 24-9-21/源.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param nums int整型vector
+     * @param target int整型
+     * @return int整型
+     */
+    int search(vector<int>& nums, int target) {
+        int len = nums.size();
+        if (len == 0) {
+            return -1;
+        }
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+        // write code here
+    }
+};
