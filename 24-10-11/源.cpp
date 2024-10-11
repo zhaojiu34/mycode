46c6e916b8946759767d94c0674a853dfd333370
zhaojiue34 <2827871771@qq.com>
2024-10-11 22:38:37 +0800

    24-10-11

---
+++ 24-10-11/源.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+ struct TreeNode {
+ 	int val;
+ 	struct TreeNode *left;
+ 	struct TreeNode *right;
+ };
+
+class Solution {
+public:
+    /**
+     *
+     * @param t1 TreeNodeÀà
+     * @param t2 TreeNodeÀà
+     * @return TreeNodeÀà
+     */
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        // write code here
+        if (!t1) {
+            return t2;
+        }
+        if (!t2) {
+            return t1;
+        }
+        TreeNode* ret = new TreeNode(t1->val + t2->val);
+        ret->left = mergeTrees(t1->left, t2->left);
+        ret->right = mergeTrees(t1->right, t2->right);
+
+        return ret;
+    }
+};
